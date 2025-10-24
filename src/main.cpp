3d911a8bdfb7141db33e743e16673c4e9007ce88
g-sawicki <sawickigrzegorz66@gmail.com>
2025-10-24 10:36:33 +0200

    VkInstance

--- src/main.cpp
+++ src/main.cpp
@@ -24,11 +24,35 @@ private:
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window_ = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", NULL, NULL);
+        window_ = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
     void initVulkan() {
+        createInstance();
+    }
 
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance_) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create a Vulkan instance!");
+        }
     }
 
     void mainLoop() {
@@ -38,12 +62,16 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance_, nullptr);
+
         glfwDestroyWindow(window_);
         glfwTerminate();
     }
 
 private:
     GLFWwindow* window_;
+
+    VkInstance instance_;
 };
 
 int main() {
