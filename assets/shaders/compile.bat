0740cc86dce30ab2b30d2fe4bfee33a8f182901f
g-sawicki <sawickigrzegorz66@gmail.com>
2025-10-30 19:36:46 +0100

    Use VULKAN_SDK in shader compilation script

--- assets/shaders/compile.bat
+++ assets/shaders/compile.bat
@@ -1,3 +1,3 @@
-C:/Dev/VulkanSDK/1.4.328.1/Bin/glslc.exe shader.vert -o out/vert.spv
-C:/Dev/VulkanSDK/1.4.328.1/Bin/glslc.exe shader.frag -o out/frag.spv
+%VULKAN_SDK%/Bin/glslc.exe shader.vert -o out/vert.spv
+%VULKAN_SDK%/Bin/glslc.exe shader.frag -o out/frag.spv
 pause
