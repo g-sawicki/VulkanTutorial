c7be8cddcf18a30dfc118017a7fd816a5606620b
grzegorz-sawicki-img <Grzegorz.Sawicki@imgtec.com>
2025-11-06 16:19:08 +0100

    Image view and sampler

--- src/main.cpp
+++ src/main.cpp
@@ -142,6 +142,8 @@ private:
         createFramebuffers();
         createCommandPool();
         createTextureImage();
+        createTextureImageView();
+        createTextureSampler();
         createVertexBuffer();
         createIndexBuffer();
         createUniformBuffers();
@@ -325,8 +327,8 @@ private:
         //VkPhysicalDeviceProperties deviceProperties;
         //vkGetPhysicalDeviceProperties(device, &deviceProperties);
 
-        //VkPhysicalDeviceFeatures deviceFeatures;
-        //vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+        VkPhysicalDeviceFeatures deviceFeatures;
+        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
         QueueFamilyIndices indices = findQueueFamilies(device);
         bool extensionsSupported = checkDeviceExtensionSupport(device);
@@ -337,7 +339,7 @@ private:
             swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
         }
 
-        return indices.isComplete() && extensionsSupported && swapChainAdequate;
+        return indices.isComplete() && extensionsSupported && swapChainAdequate && deviceFeatures.samplerAnisotropy;
     }
 
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) const {
@@ -406,6 +408,7 @@ private:
         }
 
         VkPhysicalDeviceFeatures deviceFeatures{};
+        deviceFeatures.samplerAnisotropy = VK_TRUE;
 
         VkDeviceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
@@ -581,25 +584,28 @@ private:
         swapChainImageViews_.resize(swapChainImages_.size());
 
         for (size_t i = 0; i < swapChainImages_.size(); ++i) {
-            VkImageViewCreateInfo createInfo{};
-            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-            createInfo.image = swapChainImages_[i];
-            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-            createInfo.format = swapChainImageFormat_;
-            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-            createInfo.subresourceRange.baseMipLevel = 0;
-            createInfo.subresourceRange.levelCount = 1;
-            createInfo.subresourceRange.baseArrayLayer = 0;
-            createInfo.subresourceRange.layerCount = 1;
-
-            if (vkCreateImageView(device_, &createInfo, nullptr, &swapChainImageViews_[i]) != VK_SUCCESS) {
-                throw std::runtime_error("Failed to create an image view!");
-            }
+            swapChainImageViews_[i] = createImageView(swapChainImages_[i], swapChainImageFormat_);
         }
+    }
+
+    VkImageView createImageView(VkImage image, VkFormat format) {
+        VkImageViewCreateInfo viewInfo{};
+        viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        viewInfo.image = image;
+        viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        viewInfo.format = format;
+        viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        viewInfo.subresourceRange.baseMipLevel = 0;
+        viewInfo.subresourceRange.levelCount = 1;
+        viewInfo.subresourceRange.baseArrayLayer = 0;
+        viewInfo.subresourceRange.layerCount = 1;
+
+        VkImageView imageView;
+        if (vkCreateImageView(device_, &viewInfo, nullptr, &imageView) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create an image view!");
+        }
+
+        return imageView;
     }
 
     void createDescriptorSetLayout() {
@@ -992,6 +998,37 @@ private:
         );
 
         endSingleTimeCommands(commandBuffer);
+    }
+
+    void createTextureImageView() {
+        textureImageView_ = createImageView(textureImage_, VK_FORMAT_R8G8B8A8_SRGB);
+    }
+
+    void createTextureSampler() {
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(physicalDevice_, &properties);
+
+        VkSamplerCreateInfo samplerInfo{};
+        samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+        samplerInfo.magFilter = VK_FILTER_LINEAR;
+        samplerInfo.minFilter = VK_FILTER_LINEAR;
+        samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.anisotropyEnable = VK_TRUE;
+        samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+        samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+        samplerInfo.unnormalizedCoordinates = VK_FALSE;
+        samplerInfo.compareEnable = VK_FALSE;
+        samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+        samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        samplerInfo.mipLodBias = 0.0f;
+        samplerInfo.minLod = 0.0f;
+        samplerInfo.maxLod = 0.0f;
+
+        if (vkCreateSampler(device_, &samplerInfo, nullptr, &textureSampler_) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create a texture sampler!");
+        }
     }
 
     void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height) {
@@ -1426,6 +1463,9 @@ private:
         vkDestroyBuffer(device_, indexBuffer_, nullptr);
         vkFreeMemory(device_, indexBufferMemory_, nullptr);
 
+        vkDestroySampler(device_, textureSampler_, nullptr);
+
+        vkDestroyImageView(device_, textureImageView_, nullptr);
         vkDestroyImage(device_, textureImage_, nullptr);
         vkFreeMemory(device_, textureImageMemory_, nullptr);
 
@@ -1500,6 +1540,8 @@ private:
     VkDeviceMemory indexBufferMemory_        = VK_NULL_HANDLE;
     VkImage textureImage_                    = VK_NULL_HANDLE;
     VkDeviceMemory textureImageMemory_       = VK_NULL_HANDLE;
+    VkImageView textureImageView_            = VK_NULL_HANDLE;
+    VkSampler textureSampler_                = VK_NULL_HANDLE;
 
     VkDescriptorSetLayout descriptorSetLayout_  = VK_NULL_HANDLE;
     VkDescriptorPool descriptorPool_            = VK_NULL_HANDLE;
